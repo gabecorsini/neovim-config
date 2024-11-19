59dfd5e0c7df9abaec8bb737fd328b61a4ef47ff
Gabe Corsini <gabe.corsini@gmail.com>
2024-11-19 15:34:57 -0700

    updated to latest place in neovim tutorial

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,57 @@
+return {
+    {
+        'williamboman/mason.nvim',
+        lazy = true,
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        lazy = false,
+        opts = {
+            auto_install = true,
+            ensure_installed = {"lua_ls", "ts_ls", "html", "tailwindcss", "cssls", "powershell_es", "bashls" },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+              local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+              local lspconfig = require("lspconfig")
+              lspconfig.lua_ls.setup({
+                capabilities = capabilities
+              })
+              lspconfig.html.setup({
+                capabilities = capabilities
+              })
+              lspconfig.ts_ls.setup({
+                capabilities = capabilities
+              })
+              lspconfig.tailwindcss.setup({
+                capabilities = capabilities
+              })
+              lspconfig.cssls.setup({
+                capabilities = capabilities
+              })
+              lspconfig.powershell_es.setup({
+                capabilities = capabilities
+              })
+              lspconfig.bashls.setup({
+                capabilities = capabilities
+              })
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+            vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, {})
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+            vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, {})
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+            vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, {})
+        end,
+    },
+}
