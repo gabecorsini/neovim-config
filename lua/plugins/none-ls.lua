fe89b5e61ea2d797f3cba784979755bad05771a4
Gabe Corsini <gcorsini@ardalyst.com>
2025-03-11 16:07:34 -0600

    fix null-ls for php

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,9 +10,9 @@ return {
                 require("none-ls.diagnostics.eslint_d"),
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.shfmt,
-                null_ls.builtins.diagnostics.php,
                 null_ls.builtins.formatting.blade_formatter,
                 null_ls.builtins.formatting.prettierd,
+                null_ls.builtins.formatting.pint,
             },
         })
         vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
